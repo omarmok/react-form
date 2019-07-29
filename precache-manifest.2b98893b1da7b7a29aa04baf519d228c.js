52e2a472c4f787ea0276d813989f48ecbe6f66c8
Omar Mokhtar <al_fagomy22@hotmail.com>
2019-07-29 16:20:50 +0300

    Updates

---
+++ precache-manifest.2b98893b1da7b7a29aa04baf519d228c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f69ac29fcb47cb198448e194ab742f",
+    "url": "/react-form/index.html"
+  },
+  {
+    "revision": "edd71a7e80e0bbdc9f9c",
+    "url": "/react-form/static/css/2.942673ba.chunk.css"
+  },
+  {
+    "revision": "c673d4c7cf7ddf71bbef",
+    "url": "/react-form/static/css/main.f3bd21ef.chunk.css"
+  },
+  {
+    "revision": "edd71a7e80e0bbdc9f9c",
+    "url": "/react-form/static/js/2.5f61630f.chunk.js"
+  },
+  {
+    "revision": "c673d4c7cf7ddf71bbef",
+    "url": "/react-form/static/js/main.3885410e.chunk.js"
+  },
+  {
+    "revision": "15bae7cbdef7d06c46be",
+    "url": "/react-form/static/js/runtime~main.98e38a88.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-form/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-form/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-form/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-form/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-form/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
