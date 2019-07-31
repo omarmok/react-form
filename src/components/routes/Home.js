cc9253ece0b7365cff0ca8a936bb9d88de39f8c8
Omar Mokhtar <al_fagomy22@hotmail.com>
2019-07-31 08:58:05 +0300

    className

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -12,22 +12,22 @@ class Home extends Component {
           </div>
            </div>
 
-          <div class="row">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a  class="btn btn-primary">Go somewhere</a>
+          <div className="row">
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a  className="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a  class="btn btn-primary">Go somewhere</a>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a  className="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>
